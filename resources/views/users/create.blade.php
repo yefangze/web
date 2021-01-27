3d0d1471ffdcecb15c665d695fbc0842fe05daa7
Summer <460701@qq.com>
2021-01-27 03:42:13 +0000

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -7,8 +7,11 @@
             <div class="card-header">
                 <h5>注册</h5>
             </div>
+
             <div class="card-body">
+                @include('shared._errors')
                 <form method="POST" action="{{ route('users.store') }}">
+                    {{ csrf_field() }}
                     <div class="form-group">
                         <label for="name">名称：</label>
                         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
